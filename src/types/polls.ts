f33ef787f0a354ba45e757ffefe30f339963843e
Danish Ansari <dvansari360@gmail.com>
2025-10-20 09:33:46 +0530

    added type for poll

---
+++ src/types/polls.ts
@@ -0,0 +1,21 @@
+
+
+export interface Poll {
+    pollId:number | null;
+    description:string,
+    startDate:number,
+    endDate:number
+}
+
+interface pollPdaAccount {
+    canditatesAmounts:number ,
+    startDate:number,
+    endDate:number
+    description:string,
+    pollId:number
+}
+
+export interface getAllPollsResponse {
+    account:pollPdaAccount,
+    publicKey:string
+}
