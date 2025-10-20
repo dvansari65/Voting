0d611e8f6517a02bbc164ba9704ee4a4340c5e15
Danish Ansari <dvansari360@gmail.com>
2025-10-20 15:25:53 +0530

    added laoder component

---
+++ src/components/ui/loader.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <div>
+      <div className="loader-wrapper">
+        <span className="loader-letter">F</span>
+        <span className="loader-letter">e</span>
+        <span className="loader-letter">t</span>
+        <span className="loader-letter">c</span>
+        <span className="loader-letter">h</span>
+        <span className="loader-letter">i</span>
+        <span className="loader-letter">n</span>
+        <span className="loader-letter">g</span>
+        <div className="loader"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
