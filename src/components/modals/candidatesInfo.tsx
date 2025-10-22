8633a593b8645eca9987aee21e06458d0ddfe87a
Danish Ansari <dvansari360@gmail.com>
2025-10-22 09:17:00 +0530

    fixed:fixed props

--- src/components/modals/candidatesInfo.tsx
+++ src/components/modals/candidatesInfo.tsx
@@ -1,28 +1,39 @@
+"use client"
 import { candidateInfo } from '@/types/candidate'
 import { X } from 'lucide-react'
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import {v4} from "uuid"
 interface CandidatesInfoProps {
-  candidateName: candidateInfo[]
+  candidateNames: string[]
   isOpen: boolean
   onClose: () => void
   className?: string
 }
 
-function CandidatesInfo({ candidateName, isOpen, onClose, className }: CandidatesInfoProps) {
+function CandidatesInfo({ candidateNames, isOpen, onClose, className }: CandidatesInfoProps) {
   if (!isOpen) return null
+ useEffect(()=>{
+  console.log("candidates name",candidateNames[0]);
+ },[candidateNames])
+ 
   return (
     <div
       className={`w-[500px] bg-purple-900  md:p-8 md:pt-2 md:pr-2 rounded-2xl border  border-white/20 mb-4 ${className}`}
     >
       <div className="w-full flex justify-end">
         <button onClick={onClose}>
-          <X />
+          <X/>
         </button>
       </div>
-      {candidateName.map((candidateName) => (
-        <div className=" mx-auto h-full flex flex-col justify-center">
-          <span>{candidateName?.name || 'Danish Ansari'}</span>
+      <div className='w-full flex justify-center'>
+      <span className='text-2xl font-bold mb-1'>
+          Candidates
+        </span>
+      </div>
+      
+      {candidateNames?.map((candidateName,index) => (
+        <div key={`${candidateName}-${index}`} className=" mx-auto h-full flex flex-col justify-start">
+          <span className='border border-gray-500 rounded-[5px] m-1 p-1 pl-2 font-bold'># {candidateName }</span>
         </div>
       ))}
     </div>
